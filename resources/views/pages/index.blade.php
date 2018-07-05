ec886e5a13d1ed348db2836100a31b742357886a
Javar <javarruedas@gmail.com>
2018-07-06 07:30:12 +0800

    1 to ONE

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div class="jumbotron text-center">
-        <h1>Welcome To Player 1!</h1>
+        <h1>Welcome To Player ONE !</h1>
         <p>PLAYER <b>O</b>NE puts the player <b>FIRST<b></p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
     </div>
