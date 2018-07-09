00ddd1219a19bd94975312e5c858e7e2d2ee2f50
Javar <javarruedas@gmail.com>
2018-07-09 16:41:22 +0800

    css hover test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,39 @@
 
    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
    <style>
+       .image {
+      display: block;
+      width: 100%;
+      height: auto;
+    }
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #008CBA;
+}
+
+.col-md-6:hover .overlay {
+  opacity: 0.5;
+}
+
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
 /*
 body {
 margin: 0;
